57b96bc1d76118bbaf5c172cdc0c1a8c7359cb64
sanmart02 <113845440+sanmart02@users.noreply.github.com>
2022-12-25 22:01:31 -0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,15 +3,15 @@ import os
 class Config(object):
 
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5626940574:AAHepq10llJDR3LGfln8TvuTH_cfZGCF4pg")
 
     # The Telegram API things
     # Get these values from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 9100170))
+    API_HASH = os.environ.get("8bd74422e5a71ca6a5b00bcbdb1bfa48")
 
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1170822786").split())
 
     # Ban Unwanted Members..
     BANNED_USERS = []
